339ea2eb26d32fb4786b91bd4e23f53d7243e3e5
Mario <rodriguez.ma.4@pg.com>
2022-12-17 23:54:29 -0600

    newChanges

---
+++ PutToCloudStorage/signed_url/signed_url.py
@@ -0,0 +1,110 @@
+import binascii
+import collections
+import datetime
+import hashlib
+import sys
+import json
+
+# pip install google-auth
+from google.oauth2 import service_account
+
+# pip install six
+import six
+from six.moves.urllib.parse import quote
+
+HTTP_METHOD = "PUT"
+EXPIRATION = 604800
+BUCKET_NAME = "mario-test"
+
+
+def signed_url(object_name):
+    if EXPIRATION > 604800:
+        print("Expiration Time can't be longer than 604800 seconds (7 days).")
+        sys.exit(1)
+
+    # object_name = request.args.get('file')
+
+    escaped_object_name = quote(six.ensure_binary(object_name), safe=b"/~")
+    canonical_uri = "/{}".format(escaped_object_name)
+
+    datetime_now = datetime.datetime.now(tz=datetime.timezone.utc)
+    request_timestamp = datetime_now.strftime("%Y%m%dT%H%M%SZ")
+    datestamp = datetime_now.strftime("%Y%m%d")
+
+    service_account_info = json.load(open("service_accounts_keys.json"))
+    google_credentials = service_account.Credentials.from_service_account_info(
+        service_account_info
+    )
+
+    client_email = google_credentials.service_account_email
+    credential_scope = "{}/auto/storage/goog4_request".format(datestamp)
+    credential = "{}/{}".format(client_email, credential_scope)
+
+    headers = dict()
+    host = "{}.storage.googleapis.com".format(BUCKET_NAME)
+    headers["host"] = host
+
+    canonical_headers = ""
+    ordered_headers = collections.OrderedDict(sorted(headers.items()))
+    for k, v in ordered_headers.items():
+        lower_k = str(k).lower()
+        strip_v = str(v).lower()
+        canonical_headers += "{}:{}\n".format(lower_k, strip_v)
+
+    signed_headers = ""
+    for k, _ in ordered_headers.items():
+        lower_k = str(k).lower()
+        signed_headers += "{};".format(lower_k)
+    signed_headers = signed_headers[:-1]  # remove trailing ';'
+
+    query_parameters = dict()
+    query_parameters["X-Goog-Algorithm"] = "GOOG4-RSA-SHA256"
+    query_parameters["X-Goog-Credential"] = credential
+    query_parameters["X-Goog-Date"] = request_timestamp
+    query_parameters["X-Goog-Expires"] = EXPIRATION
+    query_parameters["X-Goog-SignedHeaders"] = signed_headers
+
+    canonical_query_string = ""
+    ordered_query_parameters = collections.OrderedDict(sorted(query_parameters.items()))
+    for k, v in ordered_query_parameters.items():
+        encoded_k = quote(str(k), safe="")
+        encoded_v = quote(str(v), safe="")
+        canonical_query_string += "{}={}&".format(encoded_k, encoded_v)
+    canonical_query_string = canonical_query_string[:-1]  # remove trailing '&'
+
+    canonical_request = "\n".join(
+        [
+            HTTP_METHOD,
+            canonical_uri,
+            canonical_query_string,
+            canonical_headers,
+            signed_headers,
+            "UNSIGNED-PAYLOAD",
+        ]
+    )
+
+    canonical_request_hash = hashlib.sha256(canonical_request.encode()).hexdigest()
+
+    string_to_sign = "\n".join(
+        [
+            "GOOG4-RSA-SHA256",
+            request_timestamp,
+            credential_scope,
+            canonical_request_hash,
+        ]
+    )
+
+    # signer.sign() signs using RSA-SHA256 with PKCS1v15 padding
+    signature = binascii.hexlify(
+        google_credentials.signer.sign(string_to_sign)
+    ).decode()
+
+    scheme_and_host = "{}://{}".format("https", host)
+    signed_url = "{}{}?{}&x-goog-signature={}".format(
+        scheme_and_host, canonical_uri, canonical_query_string, signature
+    )
+    print(signed_url)
+    return signed_url
+
+
+#signed_url("mario.json")
---
+++ WorkLog/README.md
@@ -0,0 +1,5 @@
+## Work Log
+###  Saturday 17th
+- 23:10 - Cloud function is able to be executed when assigned a proper IAM Role.
+- 23:21 - To the cloud function will add the signed url code
+- 23:36 - Function not updating properly, doing it back from scratch
