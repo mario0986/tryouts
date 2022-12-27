d6af12cfeff82f4fb8e5dce17bbdd450f9bc964e
Mario <rodriguez.ma.4@pg.com>
2022-12-26 19:06:18 -0600

    oauth working

--- Experiments/oauth2/main.py
+++ Experiments/oauth2/main.py
@@ -1,52 +1,137 @@
-# Create Authorization Request
-
-# Based on this documentation
-# https://developers.google.com/identity/protocols/oauth2/service-account#python_2 section Addendum
-# Authorized Calls Using JWT Token
-# For the kid field in the header, specify your service account's private key ID. You can find this value in the private_key_id field of your service account JSON file.
-# For the iss and sub fields, specify your service account's email address. You can find this value in the client_email field of your service account JSON file.
-# For the aud field, specify the API endpoint. For example: https://SERVICE.googleapis.com/.
-# For the iat field, specify the current Unix time, and for the exp field, specify the time exactly 3600 seconds later, when the JWT will expire.
-
-# All the attributes below are taken from the ke
-# {
-#   "alg": "RS256",
-#   "typ": "JWT",
-#   "kid": "private_key_id"
-# }
-# .
-# {
-#   "iss": "client_email",
-#   "sub": "client_email",
-#   "aud": "https://storage.googleapis.com/",
-#   "iat": 1511900000,
-#   "exp": 1511903600
-# }
-
-# Article this code is based on https://developers.google.com/identity/protocols/oauth2
-# https://developers.google.com/identity/protocols/oauth2/service-account
-# https://developers.google.com/identity/protocols/oauth2/service-account#python_2 
-
-import time 
-iat = time.time()
+import time
+import json
 import jwt
-import json 
+import requests
+import httplib2
 
-token_file = open('service_accounts_keys.json')
-token_data = json.load(token_file)
+# Project ID for this request.
+project = 'programiv'
 
-exp = iat + 3600
+# The name of the zone for this request.
+zone = 'us-east1'
 
-payload = {'iss': token_data["client_email"],
-           'sub': token_data["client_email"],
-           'aud': 'https://storage.googleapis.com/',
-           'iat': iat,
-           'exp': exp}
+# Service Account Credentials, Json format
+json_filename = 'service_accounts_keys.json'
 
-additional_headers = {"kid": token_data["private_key_id"]}
-signed_jwt = jwt.encode(payload, 
-                        token_data["private_key"],
-                        headers=additional_headers,
-                        algorithm='RS256')
-print("\nSignedJWY\n",signed_jwt)
+# Permissions to request for Access Token
+scopes = "https://www.googleapis.com/auth/devstorage.read_write"
 
+# Set how long this token will be valid in seconds
+expires_in = 3600	# Expires in 1 hour
+
+def load_json_credentials(filename):
+    ''' Load the Google Service Account Credentials from Json file '''
+
+    with open(filename, 'r') as f:
+        data = f.read()
+
+    return json.loads(data)
+
+def load_private_key(json_cred):
+    ''' Return the private key from the json credentials '''
+
+    return json_cred['private_key']
+
+def create_signed_jwt(pkey, pkey_id, email, scope):
+    '''
+    Create a Signed JWT from a service account Json credentials file
+    This Signed JWT will later be exchanged for an Access Token
+    '''
+
+    # Google Endpoint for creating OAuth 2.0 Access Tokens from Signed-JWT
+    auth_url = "https://www.googleapis.com/oauth2/v4/token"
+
+    issued = int(time.time())
+    expires = issued + expires_in	# expires_in is in seconds
+
+    # Note: this token expires and cannot be refreshed. The token must be recreated
+
+    # JWT Headers
+    additional_headers = {
+            'kid': pkey_id,
+            "alg": "RS256", # Google uses SHA256withRSA
+            "typ": "JWT"
+    }
+
+    # JWT Payload
+    payload = {
+        "iss": email,		# Issuer claim
+        "sub": email,		# Issuer claim
+        "aud": auth_url,	# Audience claim
+        "iat": issued,		# Issued At claim
+        "exp": expires,		# Expire time
+        "scope": scope		# Permissions
+    }
+
+    # Encode the headers and payload and sign creating a Signed JWT (JWS)
+    sig = jwt.encode(payload, pkey, algorithm="RS256", headers=additional_headers)
+
+    return sig
+
+# This will happen on SAP
+def exchangeJwtForAccessToken(signed_jwt):
+    '''
+    This function takes a Signed JWT and exchanges it for a Google OAuth Access Token
+    '''
+
+    auth_url = "https://www.googleapis.com/oauth2/v4/token"
+
+    params = {
+        "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+        "assertion": signed_jwt
+    }    
+
+    r = requests.post(auth_url, data=params)
+    
+    if r.ok:
+        return(r.json()['access_token'], '')
+
+    return None, r.text
+    '''
+    This functions lists the Google Compute Engine Instances in one zone
+    '''
+
+    # Endpoint that we will call
+    url = "https://www.googleapis.com/compute/v1/projects/" + project + "/zones/" + zone + "/instances"
+
+    # One of the headers is "Authorization: Bearer $TOKEN"
+    headers = {
+        "Host": "www.googleapis.com",
+        "Authorization": "Bearer " + accessToken,
+        "Content-Type": "application/json"
+    }
+
+    h = httplib2.Http()
+
+    resp, content = h.request(uri=url, method="GET", headers=headers)
+
+    status = int(resp.status)
+
+    if status < 200 or status >= 300:
+        print('Error: HTTP Request failed')
+        return
+
+    j = json.loads(content.decode('utf-8').replace('\n', ''))
+
+    print('Compute instances in zone', zone)
+    print('------------------------------------------------------------')
+    for item in j['items']:
+        print(item['name'])
+
+if __name__ == '__main__':
+    cred = load_json_credentials(json_filename)
+    
+    private_key = load_private_key(cred)
+    
+    s_jwt = create_signed_jwt(
+            private_key,
+            cred['private_key_id'],
+            cred['client_email'],
+            scopes)
+
+    token, err = exchangeJwtForAccessToken(s_jwt)
+    print(token)
+
+    if token is None:
+        print('Error:', err)
+        exit(1)
