6b5185a647330c589f09a400ce93e3b3230c0677
Mario <rodriguez.ma.4@pg.com>
2022-12-26 21:13:35 -0600

    finalChangesToOAuth

--- Experiments/oauth2/main.py
+++ Experiments/oauth2/main.py
@@ -5,62 +5,72 @@ import requests
 import httplib2
 
 # Project ID for this request.
-project = 'programiv'
+project = "programiv"
 
 # The name of the zone for this request.
-zone = 'us-east1'
+zone = "us-east1"
 
 # Service Account Credentials, Json format
-json_filename = 'service_accounts_keys.json'
+json_filename = "service_accounts_keys.json"
 
 # Permissions to request for Access Token
 scopes = "https://www.googleapis.com/auth/devstorage.read_write"
 
 # Set how long this token will be valid in seconds
-expires_in = 3600	# Expires in 1 hour
+expires_in = 3600  # Expires in 1 hour
+
+class BearerAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+    def __call__(self, r):
+        r.headers["authorization"] = "Bearer " + self.token
+        return r
 
 def load_json_credentials(filename):
-    ''' Load the Google Service Account Credentials from Json file '''
+    """Load the Google Service Account Credentials from Json file"""
 
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         data = f.read()
 
     return json.loads(data)
 
+
 def load_private_key(json_cred):
-    ''' Return the private key from the json credentials '''
+    """Return the private key from the json credentials"""
 
-    return json_cred['private_key']
+    return json_cred["private_key"]
 
+
+# GCP - Cloud Function to retrieve the signed url
 def create_signed_jwt(pkey, pkey_id, email, scope):
-    '''
+    """
     Create a Signed JWT from a service account Json credentials file
     This Signed JWT will later be exchanged for an Access Token
-    '''
+    """
 
     # Google Endpoint for creating OAuth 2.0 Access Tokens from Signed-JWT
     auth_url = "https://www.googleapis.com/oauth2/v4/token"
 
     issued = int(time.time())
-    expires = issued + expires_in	# expires_in is in seconds
+    expires = issued + expires_in  # expires_in is in seconds
 
     # Note: this token expires and cannot be refreshed. The token must be recreated
 
     # JWT Headers
     additional_headers = {
-            'kid': pkey_id,
-            "alg": "RS256", # Google uses SHA256withRSA
-            "typ": "JWT"
+        "kid": pkey_id,
+        "alg": "RS256",  # Google uses SHA256withRSA
+        "typ": "JWT",
     }
 
     # JWT Payload
     payload = {
-        "iss": email,		# Issuer claim
-        "sub": email,		# Issuer claim
-        "aud": auth_url,	# Audience claim
-        "iat": issued,		# Issued At claim
-        "exp": expires,		# Expire time
-        "scope": scope		# Permissions
+        "iss": email,  # Issuer claim
+        "sub": email,  # Issuer claim
+        "aud": auth_url,  # Audience claim
+        "iat": issued,  # Issued At claim
+        "exp": expires,  # Expire time
+        "scope": scope,  # Permissions
     }
 
     # Encode the headers and payload and sign creating a Signed JWT (JWS)
@@ -68,70 +78,56 @@ def create_signed_jwt(pkey, pkey_id, email, scope):
 
     return sig
 
-# This will happen on SAP
+
+# SAP - Exchange Token With Signed JWT
 def exchangeJwtForAccessToken(signed_jwt):
-    '''
+    """
     This function takes a Signed JWT and exchanges it for a Google OAuth Access Token
-    '''
+    """
 
     auth_url = "https://www.googleapis.com/oauth2/v4/token"
 
     params = {
         "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
-        "assertion": signed_jwt
-    }    
-
-    r = requests.post(auth_url, data=params)
-    
-    if r.ok:
-        return(r.json()['access_token'], '')
-
-    return None, r.text
-    '''
-    This functions lists the Google Compute Engine Instances in one zone
-    '''
-
-    # Endpoint that we will call
-    url = "https://www.googleapis.com/compute/v1/projects/" + project + "/zones/" + zone + "/instances"
-
-    # One of the headers is "Authorization: Bearer $TOKEN"
-    headers = {
-        "Host": "www.googleapis.com",
-        "Authorization": "Bearer " + accessToken,
-        "Content-Type": "application/json"
+        "assertion": signed_jwt,
     }
 
-    h = httplib2.Http()
+    r = requests.post(auth_url, data=params)
 
-    resp, content = h.request(uri=url, method="GET", headers=headers)
+    if r.ok:
+        return (r.json()["access_token"], "")
 
-    status = int(resp.status)
+    return None, r.text
 
-    if status < 200 or status >= 300:
-        print('Error: HTTP Request failed')
-        return
 
-    j = json.loads(content.decode('utf-8').replace('\n', ''))
+# # SAP - PUT File into GCP
+def putFileIntoCloudStorage(access_token):
+    """
+    This function takes an Access Token to perform a PUT operation into GCP Cloud Storage
+    url = https://storage.googleapis.com/{Bucket}/{file}
+    """
 
-    print('Compute instances in zone', zone)
-    print('------------------------------------------------------------')
-    for item in j['items']:
-        print(item['name'])
+    url = "https://storage.googleapis.com/mario-test/draft.json"
 
-if __name__ == '__main__':
+    fileName = open("draft.json")
+    headers = {"Content-type": "multipart/form-data"}
+
+    r = requests.put(url, auth = BearerAuth(access_token), data = fileName)
+
+
+if __name__ == "__main__":
     cred = load_json_credentials(json_filename)
-    
+
     private_key = load_private_key(cred)
-    
+
     s_jwt = create_signed_jwt(
-            private_key,
-            cred['private_key_id'],
-            cred['client_email'],
-            scopes)
+        private_key, cred["private_key_id"], cred["client_email"], scopes
+    )
 
     token, err = exchangeJwtForAccessToken(s_jwt)
-    print(token)
+    
+    putFileIntoCloudStorage(token)
 
     if token is None:
-        print('Error:', err)
+        print("Error:", err)
         exit(1)
